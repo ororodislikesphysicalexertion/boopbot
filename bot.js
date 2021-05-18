20f0bab0d1b6fb8cfc7d0863aacad408d6fc2003
ororo the unicorn <45035470+ororodislikesphysicalexertion@users.noreply.github.com>
2021-05-18 12:04:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const Discord = require('discord.js');
 const {prefix, token} = require('./config.json');
+const fetch = require("node-fetch");
 
 const client = new Discord.Client();
 
@@ -8,7 +9,7 @@ client.once('ready', () => {
 	console.log('Ready!');
 });
 
-client.on('message', message => {
+client.on('message', async message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
@@ -19,9 +20,12 @@ client.on('message', message => {
 		var imageNumber = Math.floor(Math.random() * totalImages) + 1;
 		message.channel.send({files: ["./images/" + imageNumber + ".jpg"]});
 	} else if(command === "pet") {
-		var totalGifs = 5;
+		var totalGifs = 20;
 		var gifNumber = Math.floor(Math.random() * totalGifs) + 1;
 		message.channel.send({files: ["./gifs/" + gifNumber + ".gif"]});
+	} else if (command == "cat") {
+		const { file } = await fetch("https://aws.random.cat/meow").then(response => response.json());
+		message.channel.send(file);
 	}
 	
 });
